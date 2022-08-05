103b4fea39434744f8cfdef1ea06bb1b0fd2ea75
TFMahabub <rjmahabub543@gmail.com>
2022-08-05 15:41:53 +0600

    one function many result

---
+++ one-function-many-result.js
@@ -0,0 +1,19 @@
+function inchToFeet(inch){
+  const feet = 12;
+  result = inch / feet;
+  return result;
+}
+
+let dadaInch = 114;
+dadaFeet = inchToFeet(dadaInch);
+console.log(dadaFeet);
+
+
+let dadiInch = 220;
+dadiFeet = inchToFeet(dadiInch);
+console.log(dadiFeet);
+
+
+let fatherInch = 440;
+fatherFeet = inchToFeet(fatherInch);
+console.log(fatherFeet);
--- return.js
+++ return.js
@@ -1,8 +1,35 @@
-function add(num1, num2){
-  console.log(num1, num2);
-  sum = num1 + num2;
-  return sum;
-}
+// function add(num1, num2){
+//   console.log(num1, num2);
+//   sum = num1 + num2;
+//   return sum;
+// }
 
-add(148,245);
-console.log(sum);
+// sum = add(148,245);
+// console.log(sum);
+
+
+
+// allNumber[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for( i = 0; i < allNumber.length; i++){
+//   num = allNumber[i];
+//   // result = (13 * num);
+//   console.log(num);
+// }
+
+
+// var allNumber = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+// for( i = 0; i < allNumber.length; i++){
+
+//   namta13 = allNumber[i];
+
+//   result = namta13 * 13;
+
+//   console.log(result);
+// }
+
+
+
+
+
